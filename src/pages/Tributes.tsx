e83e076d9d68e1ca70f57271db48f55293ffed50
Mike <chukwumichael70@gmail.com>
2024-06-07 09:19:59 +0100

    minors

--- src/pages/Tributes.tsx
+++ src/pages/Tributes.tsx
@@ -1,18 +1,19 @@
 import { onSnapshot } from "firebase/firestore";
 import { useEffect, useState } from "react";
+import { Autoplay } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { tributesQuery } from "../contexts/DB";
-import { Autoplay } from "swiper/modules";
 
-import "swiper/css";
-import { Link } from "react-router-dom";
 import { ChevronLeftIcon } from "@heroicons/react/20/solid";
+import { useNavigate } from "react-router-dom";
+import "swiper/css";
 
 type tribute = { value: string; name: string; createdAt: Date };
 
 const Tributes = () => {
 	const [tributes, setTributes] = useState<tribute[]>([]);
 	const [loading, setLoading] = useState(true);
+	const navigate = useNavigate();
 	useEffect(() => {
 		onSnapshot(tributesQuery, (snapshot) => {
 			if (!snapshot.empty) {
@@ -27,12 +28,12 @@ const Tributes = () => {
 	});
 	return (
 		<>
-			<Link
-				to={"/"}
-				className="absolute top-4 left-4 z-50 bg-purple-950 shadow h-10 w-10 text-white rounded-full flex items-center justify-center"
+			<button
+				onClick={() => navigate(-1)}
+				className="absolute top-4 left-4 z-50 bg-purple-950 shadow h-10 w-10 text-white rounded-full flex items-center justify-center opacity-50 hover:opacity-100 transition"
 			>
 				<ChevronLeftIcon className="h-6" />
-			</Link>
+			</button>
 			{loading ? (
 				<div className="text-2xl font-bold flex justify-center items-center h-full absolute w-full animate-pulse">
 					Loading...
@@ -40,7 +41,7 @@ const Tributes = () => {
 			) : (
 				<Swiper
 					centeredSlides
-					slidesPerView={1.1}
+					slidesPerView={1}
 					spaceBetween={16}
 					loop
 					autoplay={{
